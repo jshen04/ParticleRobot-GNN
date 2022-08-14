32cd920ab78b91f8c8bb8c8b4b053bc5025c798f
Jeremy Shen <jshen20@stuy.edu>
2022-08-14 13:55:33 -0400

    evaluate imitation nn

---
+++ evaluate_imitation.py
@@ -0,0 +1,25 @@
+import torch
+import numpy as np
+
+from sim import GraphSimulator
+from vanilla_imitation import ImitationNet
+
+numbots = 9
+simulator = GraphSimulator(num=numbots, start=(300, 300), goal=(0, 0), visualizer=None)
+
+net = ImitationNet(features=6)
+net.load_state_dict(torch.load("./models/imitation_v0.pt"))
+
+episodesteps = 2500
+for step in range(episodesteps):
+    totalSteps, actions = simulator.wavePolicy()
+    for j in range(totalSteps):
+        data = simulator.step(actions[j])
+        out = net(data)
+
+        out = torch.argmax(out, dim=1)
+        print("Truth: ", actions[j])
+        print("Prediction: ", out)
+
+        for _ in range(9):
+            simulator.step(actions[j])
